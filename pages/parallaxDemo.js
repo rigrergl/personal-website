68386bfd5741296f9df0d62df8e81a0b298f3d57
Rigre Garciandia <rigrergl@outlook.com>
2021-02-22 21:56:22 -0600

    added the parallax demo as a reference

---
+++ pages/parallaxDemo.js
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import styles from "../styles/ParallaxDemo.module.scss";
+import { useState, useEffect } from "react";
+
+export default function ParallaxDemo() {
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const renderContent = () => (
+    <>
+      <div className={styles.Parallax__content__heading}>
+        <h1 className={styles.Parallax__content__heading__text}>Closure</h1>
+        <h2 className={styles.Parallax__content__heading__caption}>
+          Your one-stop source of Web Development tricks
+        </h2>
+      </div>
+      <div className={styles.Parallax__content__cta}>
+        <p>
+          <b>1. Like the video.</b> Because it helps me and my channel
+        </p>
+        <p>
+          <b>2. Like the video.</b> To see more content like that!
+        </p>
+        <p>
+          <b>3. Follow the Github link.</b> And play with this code yourself!
+        </p>
+      </div>
+    </>
+  );
+
+  return (
+    <section className={styles.Parallax}>
+    <div
+      className={styles.Parallax__background}
+      style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
+    />
+    <div
+      className={styles.Parallax__background_triangles}
+      style={{ transform: `translateY(${offsetY * 0.8}px)` }}
+    />
+    <div className={styles.Parallax__content}>{renderContent()}</div>
+  </section>
+  );
+}
